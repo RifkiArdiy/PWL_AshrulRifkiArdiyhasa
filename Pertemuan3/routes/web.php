44a3f4a31f4f9193f6e88e090952da6eced171ad
RifkiArdiy1~ <ardiyrifki@gmail.com>
2025-03-06 11:39:15 +0700

    D. DB Facade Jobsheet 3

--- Pertemuan3/routes/web.php
+++ Pertemuan3/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
@@ -32,4 +33,4 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('use
 
 Route::get('/sales', [SalesController::class, 'index'])->name('sales.pos');
 
-
+Route::get('/level', [LevelController::class, 'index']);
