3a00472725fcd2ad9cdd3e2312266e3a954aed29
RifkiArdiy1~ <ardiyrifki@gmail.com>
2025-03-11 22:23:14 +0700

    Praktikum 2.1 s/d 2.7 Jobsheet 4

--- Pertemuan4/routes/web.php
+++ Pertemuan4/routes/web.php
@@ -37,3 +37,11 @@ Route::get('/sales', [SalesController::class, 'index'])->name('sales.pos');
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+
+
+// ------------------------------------------ Jobsheet 4 ------------------------------------------
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::get('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
