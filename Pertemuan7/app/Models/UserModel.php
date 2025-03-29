cb1b5ebbfc23d030249542f1522e10ced542cb0b
RifkiArdiy1~ <ardiyrifki@gmail.com>
2025-03-29 14:11:07 +0700

    Praktikum 1 – Implementasi Authentication : Jobsheet 7

--- Pertemuan7/app/Models/UserModel.php
+++ Pertemuan7/app/Models/UserModel.php
@@ -6,18 +6,21 @@ use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable ;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan oleh model ini
 
     protected $fillable = [
-        'level_id',
         'username',
+        'password',
         'nama',
-        'password'
+        'level_id',
+        'created_at',
+        'updated_at'
     ]; // Mendefinisikan kolom yang dapat diisi oleh model ini
 
     public function level(): BelongsTo
